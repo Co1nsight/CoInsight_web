29fe796db6ff53d46898e600a00a86ab5da192ee
Juyoung03 <iris031008@naver.com>
2026-02-08 23:19:56 +0900

    [MOD] 코인 상세 정보 api 연결

--- src/pages/CoinDetailPage.jsx
+++ src/pages/CoinDetailPage.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/common/Navbar";
 import CoinChart from "../components/CoinDetail/CoinChart";
 import History from "../components/CoinDetail/History";
-//import { getCoinInfo } from "../apis/CoinDetail/coinInfo";
+import { getCoinInfo } from "../apis/CoinDetail/coinInfo";
 
 const CoinDetailPage = () => {
-    const coinName = "KRW-BTC";
     const ticker = "BTC";
-    const [currentPrice, setCurrentPrice] = useState(0);
-    const [tradePrice, setTradePrice] = useState(0);
-    const [fluctuationRange, setFluctuationRange] = useState(0);
-    const [isPositive, setIsPositive] = useState(true);
-    const [todayDate, setTodayDate] = useState(null);
-    const [todayTime, setTodayTime] = useState(null);
+
+    const [coinInfo, setCoinInfo] = useState({
+        name: "",
+        ticker1: "",
+        currentPrice: "0",
+        tradePrice: "0",
+        fluctuationRange: "0",
+        isPositive: true,
+        date: "",
+        time: ""
+    })
     
 
     const formatPrice = (value) => {
@@ -21,44 +25,28 @@ const CoinDetailPage = () => {
         return `${formatPrice.toLocaleString(undefined, {maximumFractionDigits: 1})}억`;
     }
 
-    const getDateTime = () => {
-        var today = new Date();
-
-        var year = today.getFullYear();
-        var month = ('0' + (today.getMonth() + 1)).slice(-2);
-        var day = ('0' + today.getDate()).slice(-2);
-
-        setTodayDate(year + '년 ' + month  + '월 ' + day + '일');
-
-        var hours = ('0' + today.getHours()).slice(-2); 
-        var minutes = ('0' + today.getMinutes()).slice(-2);
-        var seconds = ('0' + today.getSeconds()).slice(-2); 
-
-        setTodayTime(hours + ':' + minutes  + ':' + seconds);
-    }
-
     useEffect (() => {
         const options = {method: 'GET', headers: {accept: 'application/json'}};
 
-        fetch(`https://api.bithumb.com/v1/ticker?markets=${coinName}`, options)
-            .then(response => response.json())
-            .then(response => {
-                //console.log(response);
-                const formattedPrice = formatPrice(response[0].acc_trade_price_24h);
-                setTradePrice(formattedPrice);
-                const p = response[0].trade_price.toLocaleString('ko-KR');
-                setCurrentPrice(p);
-            })
-            .catch(err => console.error(err));
+        const fetchCoinInfo = async () => {
+            try {
+                const res = await getCoinInfo(ticker);
+                const {data, timestamp} = res;
+                const today = new Date(timestamp);
 
-        // const fetchCoinInfo = async () => {
-        //     try {
-        //         const res = await getCoinInfo(ticker);
-        //         console.log(res);
-        //     } catch (error) {
-        //         console.error("데이터 호출 실패 : ", error);
-        //     }
-        // }
+                setCoinInfo(prev => ({
+                    ...prev,
+                    name: data.name,
+                    ticker1: data.ticker,
+                    currentPrice: Number(data.currentPrice).toLocaleString('ko-KR'),
+                    tradePrice: formatPrice(data.tradingVolume),
+                    date: `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`,
+                    time: `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+                }));
+            } catch (error) {
+                console.error("데이터 호출 실패 : ", error);
+            }
+        }
 
         fetch(`https://api.bithumb.com/public/candlestick/BTC_KRW/1h`, options)
             .then(res => res.json())
@@ -69,14 +57,16 @@ const CoinDetailPage = () => {
                 const prevPrice = parseFloat(data[len - 2][2]);
 
                 const changeRate = ((currentPrice - prevPrice) / prevPrice) * 100;
-                setFluctuationRange(changeRate.toFixed(2));
-                if (changeRate < 0) setIsPositive(false);
+                setCoinInfo(prevInfo => ({
+                    ...prevInfo,
+                    fluctuationRange: changeRate.toFixed(2),
+                    isPositive: changeRate >= 0
+                }));
             })
             .catch(err => console.error(err));
 
-        //fetchCoinInfo();
-        getDateTime();
-    }, [coinName, ticker]);
+        fetchCoinInfo();
+    }, [ticker]);
 
     return  (
         <div>
@@ -85,35 +75,34 @@ const CoinDetailPage = () => {
                 <div className="border border-[#E0E0E0] bg-[#FFFFFF] h-35 p-8 rounded-sm items-center justify-between flex flex-row">
                     <div className="flex flex-col">
                         <div className="text-[36px] font-bold text-[#212121]">
-                            비트코인(BTC)
+                            {coinInfo.name}({coinInfo.ticker1})
                         </div>
 
                         <div className="text-[16px] text-[#787878]">
-                            {todayDate} {todayTime} 기준
+                            {coinInfo.date} {coinInfo.time} 기준
                         </div>
                     </div>
 
                     <div className="flex flex-row gap-16">
                         <div className="flex flex-col gap-2">
                             <p className="text-[14px] text-[#787878]">현재가</p>
-                            <p className="text-[28px] font-bold">₩{currentPrice}</p>
+                            <p className="text-[28px] font-bold">₩{coinInfo.currentPrice}</p>
                         </div>
 
                         <div className="flex flex-col gap-2">
                             <p className="text-[14px] text-[#787878]">1시간 등락</p>
-                            <p className={`text-[28px] font-bold ${isPositive ? `text-[#FF4242]` : `text-[#4073FF]`}`}>{fluctuationRange}%</p>
+                            <p className={`text-[28px] font-bold ${coinInfo.isPositive ? `text-[#FF4242]` : `text-[#4073FF]`}`}>{coinInfo.fluctuationRange}%</p>
                         </div>
 
                         <div className="flex flex-col gap-2">
                             <p className="text-[14px] text-[#787878]">24시간 거래대금</p>
-                            <p className="text-[28px] font-bold">₩{tradePrice}</p>
+                            <p className="text-[28px] font-bold">₩{coinInfo.tradePrice}</p>
                         </div>
                     </div>
                 </div>
 
                 <History />
-
-                <CoinChart coinName={coinName}/>
+                <CoinChart coinName={"KRW-BTC"}/>
             </div>
         </div>
     )
